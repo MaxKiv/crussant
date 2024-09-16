fa791acde77f64b6e0b5508dcaafdf01d2c3ea93
MaxKiv <maxkivits42@gmail.com>
2024-09-16 05:34:23 +0200

    feat(arm): update flake with mediocre way to select arm toolchain

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 # Documentation:
 # main source for flake.nix
 # https://gburghoorn.com/posts/just-nix-rust-wasm/
-# 
+#
 # nixpkgs manual on rust
 # https://nixos.org/manual/nixpkgs/stable/#rust
-# 
+#
 # auditable = false fix for arm-none-eabi-ld: unrecognized option '-Wl,--undefined=AUDITABLE_VERSION_INFO'
 # https://git.m-labs.hk/M-Labs/zynq-rs/commit/91bae572f913abc2f95acb899ca5afa33eeaa036#diff-58cb4f58166586c1ed7f076c568d41682df3661c
 #
 # Other embedded rust nix builds:
 # https://github.com/TwentyTwoHW/portal-software/blob/b8c4e27c138d3c980d051b8eb2a61fbc27604685/flake.nix
-# 👉 https://github.com/oddlama/nrf-template/blob/0db6cfee33cd1557517b90efbf248b486d2d247f/flake.nix 
+# 👉 https://github.com/oddlama/nrf-template/blob/0db6cfee33cd1557517b90efbf248b486d2d247f/flake.nix
 {
   description = "Embedded Hello world rust program cross compiled with nix";
 
@@ -43,27 +43,30 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # advisory-db = {
-    #   url = "github:rustsec/advisory-db";
-    #   flake = false;
-    # };
-
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, devshell, ... } @ inputs:
-
-    flake-utils.lib.eachDefaultSystem (localSystem:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    rust-overlay,
+    devshell,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      localSystem: let
         inherit (pkgs) lib;
 
         projectName = "crussant";
 
         # Replace with the system you want to build for
-        # crossSystem = "thumbv7m-none-eabi";
-        crossSystem = "riscv32imc-unknown-none-elf";
+        cross_system_esp = "riscv32imc-unknown-none-elf";
+        cross_system_arm = "thumbv7m-none-eabi";
 
         # Qemu binary required to simulate the above system
-        qemu_binary = "qemu-system-riscv32";
+        qemu_binary_esp = "qemu-system-riscv32";
+        qemu_binary_arm = "qemu-system-arm";
 
         pkgs = import nixpkgs {
           inherit localSystem;
@@ -73,16 +76,22 @@
           ];
         };
 
-        # rustToolchain = pkgs.pkgsBuildHost.rust-bin.nightly.latest.default.override {
-        #   extensions = [ "llvm-tools-preview rust-src" ];
-        #   targets = [ crossSystem ];
-        # };
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-          extensions = [ "llvm-tools-preview" "rust-src" ];
-          targets = [ crossSystem ];
-        });
+        rust_toolchain_extentions = ["llvm-tools-preview" "rust-src"];
 
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        rust_toolchain_esp = pkgs.pkgsBuildHost.rust-bin.selectLatestNightlyWith (toolchain:
+          toolchain.default.override {
+            extensions = rust_toolchain_extentions;
+            targets = [cross_system_esp];
+          });
+        rust_toolchain_arm = pkgs.pkgsBuildHost.rust-bin.selectLatestNightlyWith (toolchain:
+          toolchain.default.override {
+            extensions = rust_toolchain_extentions;
+            targets = [cross_system_esp];
+          });
+        default_rust_toolchain = rust_toolchain_esp;
+
+        # TODO add ability for crane to take ARM toolchain here
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust_toolchain_esp;
 
         # For each of the classical cargo "functions" like build, doc, test, ...,
         # crane exposes a function that takes some configuration arguments.
@@ -126,12 +135,14 @@
           # script can find the location of openssl. Note that we don't
           # need to specify the rustToolchain here since it was already
           # overridden above.
-          nativeBuildInputs = [
-            # pkg-config
-            # pkgs.gcc-arm-embedded
-          ] ++ lib.optionals pkgs.stdenv.buildPlatform.isDarwin [
-            pkgs.libiconv
-          ];
+          nativeBuildInputs =
+            [
+              # pkg-config
+              # pkgs.gcc-arm-embedded
+            ]
+            ++ lib.optionals pkgs.stdenv.buildPlatform.isDarwin [
+              pkgs.libiconv
+            ];
 
           # BUG:: This should not be disabled, but some dependencies try to compile against
           # the test crate when it isn't available...
@@ -143,7 +154,7 @@
 
           # Tell cargo which target we want to build (so it doesn't default to the build system).
           # We can either set a cargo flag explicitly with a flag or with an environment variable.
-          cargoExtraArgs = "--target ${crossSystem}";
+          cargoExtraArgs = "--target ${cross_system_esp}";
           # CARGO_BUILD_TARGET = "aarch64-unknown-linux-gnu";
 
           # Tell cargo about the linker and an optional emulater. So they can be used in `cargo build`
@@ -152,7 +163,9 @@
           # They are also be set in `.cargo/config.toml` instead.
           # See: https://doc.rust-lang.org/cargo/reference/config.html#target
           CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.stdenv.cc.targetPrefix}cc";
-          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "${qemu_binary}";
+
+          # TODO add ability to take ARM qemu here
+          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "${qemu_binary_esp}";
 
           # This environment variable may be necessary if any of your dependencies use a
           # build-script which invokes the `cc` crate to build some other code. The `cc` crate
@@ -161,60 +174,7 @@
           # system.
           HOST_CC = "${pkgs.stdenv.cc.nativePrefix}cc";
         };
-
-        # # Build *just* the cargo dependencies, so we can reuse
-        # # all of that work (e.g. via cachix) when running in CI
-        # cargoArtifacts = craneLib.buildDepsOnly (commonArgs
-        #   // {
-        #   extraDummyScript = ''
-        #     cp -a ${./memory.x} $out/memory.x
-        #   '';
-        # });
-
-        # Build the actual package
-        # package = craneLib.buildPackage (commonArgs
-        #   // {
-        #   inherit cargoArtifacts;
-        # });
-      in
-      {
-        # checks = {
-        #   # Build the crate normally as part of checking, for convenience
-        #   ${projectName} = package;
-        #
-        #   # Run clippy (and deny all warnings) on the crate source,
-        #   # again, resuing the dependency artifacts from above.
-        #   #
-        #   # Note that this is done as a separate derivation so that
-        #   # we can block the CI if there are issues here, but not
-        #   # prevent downstream consumers from building our crate by itself.
-        #   "${projectName}-clippy" = craneLib.cargoClippy (commonArgs
-        #     // {
-        #     inherit cargoArtifacts;
-        #     cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-        #   });
-        #
-        #   "${projectName}-doc" = craneLib.cargoDoc (commonArgs
-        #     // {
-        #     inherit cargoArtifacts;
-        #   });
-        #
-        #   # Check formatting
-        #   "${projectName}-fmt" = craneLib.cargoFmt {
-        #     inherit (commonArgs) src;
-        #   };
-        #
-        #   # # Audit dependencies
-        #   # "${projectName}-audit" = craneLib.cargoAudit {
-        #   #   inherit (commonArgs) src;
-        #   #   inherit advisory-db;
-        #   # };
-        #
-        # };
-
-        # packages.default = package; # `nix build`
-        # packages.${projectName} = package; # `nix build .#${projectName}`
-
+      in {
         # `nix run`
         # apps.default = flake-utils.lib.mkApp {
         #   drv = pkgs.writeScriptBin "my-app" ''
@@ -253,24 +213,23 @@
           ];
 
           # devshell.startup.pre-commit.text = self.checks.${localSystem}.pre-commit.shellHook;
-          packages =
-            let
-              # Do not expose rust's gcc: https://github.com/oxalica/rust-overlay/issues/70
-              # Create a wrapper that only exposes $pkg/bin. This prevents pulling in
-              # development deps, like python interpreter + $PYTHONPATH, when adding
-              # packages to a nix-shell. This is especially important when packages
-              # are combined from different nixpkgs versions.
-              mkBinOnlyWrapper = pkg:
-                pkgs.runCommand "${pkg.pname}-${pkg.version}-bin" { inherit (pkg) meta; } ''
-                  mkdir -p "$out/bin"
-                  for bin in "${lib.getBin pkg}/bin/"*; do
-                      ln -s "$bin" "$out/bin/"
-                  done
-                '';
-            in
-              commonArgs.buildInputs
+          packages = let
+            # Do not expose rust's gcc: https://github.com/oxalica/rust-overlay/issues/70
+            # Create a wrapper that only exposes $pkg/bin. This prevents pulling in
+            # development deps, like python interpreter + $PYTHONPATH, when adding
+            # packages to a nix-shell. This is especially important when packages
+            # are combined from different nixpkgs versions.
+            mkBinOnlyWrapper = pkg:
+              pkgs.runCommand "${pkg.pname}-${pkg.version}-bin" {inherit (pkg) meta;} ''
+                mkdir -p "$out/bin"
+                for bin in "${lib.getBin pkg}/bin/"*; do
+                    ln -s "$bin" "$out/bin/"
+                done
+              '';
+          in
+            commonArgs.buildInputs
             ++ [
-              (mkBinOnlyWrapper rustToolchain)
+              (mkBinOnlyWrapper default_rust_toolchain)
               pkgs.cargo-espflash # Serial flasher utilities for Espressif devices, based loosely on esptool.py.
               pkgs.probe-rs # Cross platform flashing & debugging utilities, cargo extention from the probe.rs group
               pkgs.gdb
@@ -283,4 +242,3 @@
       }
     );
 }
-
